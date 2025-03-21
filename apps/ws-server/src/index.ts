a906a6e7d3b31aeca16ddeed0551fe20bb3ecfa9
fuzailansariii <fuzailansarisecret@gmail.com>
2025-03-21 15:00:11 +0530

    ws-init

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,11 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("message", (message) => {
+    console.log("Received message:", message);
+    ws.send("Pong");
+  });
+});
